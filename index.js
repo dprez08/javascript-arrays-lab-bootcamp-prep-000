9e3fac26d43e86bc699cccd925e0c66f530244ca
David Perez <davperez89@gmail.com>
2017-12-14 06:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ("Milo","Otis","Garfield");
 
-function destructivelyAppendKitten(array,element){
-  kittens.push(element);
+function destructivelyAppendKitten(name){
+  kittens.push(name);
   return kittens;
 }
