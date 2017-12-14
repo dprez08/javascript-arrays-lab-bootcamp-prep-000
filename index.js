84f5ecd8aa5d000deebe07e39ec1063604b69612
David Perez <davperez89@gmail.com>
2017-12-14 06:52:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(name){
-  return name.slice(0,name.length(3);
+  return name.slice(0,name.length - 3);
 }
 
 function removeFirstKitten(name){
